fbd39eac708a34ea62e3b24fa021740ac6bf9bc1
Filip Lasic <filip.lasic89@gmail.com>
2025-11-13 17:52:28 +0100

    script runs

---
+++ script/SingletonAdapter.s.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+ 
+import {Script, console} from "forge-std/Script.sol";
+import {MorphoMarketV1SingletonAdapter} from "../src/adapters/MorphoMarketV1SingletonAdapter.sol";
+import { VaultV2 } from "src/VaultV2.sol";
+ 
+contract MorphoMarketV1SingletonAdapterScript is Script {
+ 
+    function run() public {
+        vm.startBroadcast();
+
+        VaultV2 vault = VaultV2(0xC684eF73A0522046C6F5b4d9db42012e623EBCc1);
+        address adapter = 0x5c6456672F488cef46357AA1FA611CC97017C55c;
+
+        address owner = 0xB5ea370779Ec64a2E7d68640f7457249d7cf7eB7;
+
+        vault.setCurator(owner);
+
+        bytes memory addAdapterCalldata = abi.encodeWithSelector(VaultV2.addAdapter.selector, adapter);
+
+        bytes[] memory data = new bytes[](2);
+        data[0] = abi.encodeWithSelector(VaultV2.submit.selector, addAdapterCalldata);
+        data[1] = addAdapterCalldata;
+
+        vault.multicall(data);
+ 
+        vm.stopBroadcast();
+    }
+}
